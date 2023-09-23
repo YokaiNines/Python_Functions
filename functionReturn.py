9018640a9a5e750cc2fc359dff1f7c930dd33483
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-23 12:09:41 -0400

    Add files via upload

---
+++ functionReturn.py
@@ -0,0 +1,8 @@
+def greet():
+    return "Hello, world!"
+
+# Calling the greet function and storing the result in a variable
+greeting_message = greet()
+
+# Printing the result
+print(greeting_message)
