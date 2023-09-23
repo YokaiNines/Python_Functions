d2ee7101af2a4a7b24be934d3d51c60708f74946
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-23 14:29:04 -0400

    Add files via upload

---
+++ functionParameters.py
@@ -0,0 +1,10 @@
+# Define a function with two parameters
+def add_numbers(x, y):
+    result = x + y
+    return result
+
+# Call the function with values for the parameters
+sum_result = add_numbers(5, 3)
+
+# Print the result
+print("The sum is:", sum_result)
